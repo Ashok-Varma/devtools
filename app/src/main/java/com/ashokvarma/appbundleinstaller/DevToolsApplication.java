146d03a0c73be38d4d4ef492c241d6749b595f61
Ashok Varma <ashokvitece@gmail.com>
2018-11-09 22:11:22 +0530

    Bundle tool imported as project from fine control
    Fixed these issues
    >update protobuf plugin
    >android.enableD8.desugaring = true
    >android.enableIncrementalDesugaring = false

---
+++ app/src/main/java/com/ashokvarma/appbundleinstaller/DevToolsApplication.java
@@ -0,0 +1,13 @@
+package com.ashokvarma.appbundleinstaller;
+
+import androidx.multidex.MultiDexApplication;
+
+/**
+ * Class description
+ *
+ * @author ashok
+ * @version 1.0
+ * @since 09/11/18
+ */
+public class DevToolsApplication extends MultiDexApplication {
+}
